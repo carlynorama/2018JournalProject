cb9110db033365a242662201d0d02a3421634fb4
carlynorama <carlynorama@gmail.com>
2018-02-10 15:04:28 -0800

    items only parse

--- 02_february/python_3/fileparser.py
+++ 02_february/python_3/fileparser.py
@@ -1,19 +1,32 @@
 # What the heck
 
-item_line_start = "- "
+item_line_start = "-"
 
 def loaddatafromfile(data_file_path):
+    returnarray = []
     with open(data_file_path, "r") as data_file:
         rawlinedata = [line.strip() for line in data_file]
         print(rawlinedata)
-        returnarray = []
-        for entry in rawlinedata:
+    for entry in rawlinedata:
+        if entry[0] == item_line_start:
             cleanedentry = getitemname(entry)
             returnarray.append(cleanedentry)
-        return returnarray
+    return returnarray
 
 #cleaner
 def getitemname(rawitem):
-    clean_name = rawitem.lstrip(item_line_start)
+    clean_name = rawitem[2:]
     print("|" + clean_name + "|")
     return clean_name
+
+#def f(x):
+#    return {
+#        'a': 1,
+#        'b': 2,
+#    }[x]
+
+#####def f(x):
+####    return {
+###        'a': 1,
+##        'b': 2
+#    }.get(x, 9)    # 9 is default if x not found
