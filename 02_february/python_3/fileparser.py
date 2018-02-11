7a10495bbb5394628089322595665c83dbd5cfed
carlynorama <carlynorama@gmail.com>
2018-02-10 20:56:16 -0800

    hardparse, line ->dictionary

--- 02_february/python_3/fileparser.py
+++ 02_february/python_3/fileparser.py
@@ -1,6 +1,7 @@
 # What the heck
 
-item_line_start = "-"
+import re
+special="-#|[]()@"
 
 def loadnamesonlyfromfile(data_file_path):
     returnarray = []
@@ -8,7 +9,7 @@ def loadnamesonlyfromfile(data_file_path):
         rawlinedata = [line.strip() for line in data_file]
         print(rawlinedata)
     for entry in rawlinedata:
-        if entry[0] == item_line_start:
+        if entry and entry[0] == special[0]:
             cleanedentry = getitemname(entry)
             returnarray.append(cleanedentry)
     return returnarray
@@ -19,8 +20,8 @@ def loaddatafromfile(data_file_path):
         rawlinedata = [line.strip() for line in data_file]
         print(rawlinedata)
     for entry in rawlinedata:
-        if entry[0] == item_line_start:
-            cleanedentry = getitemname(entry)
+        if entry and entry[0] == special[0]:
+            cleanedentry = getitemdictionary(entry)
             returnarray.append(cleanedentry)
     return returnarray
 
@@ -29,6 +30,81 @@ def getitemname(rawitem):
     clean_name = rawitem[2:]
     print("|" + clean_name + "|")
     return clean_name
+
+#cleaner
+def getitemdictionary(rawitem):
+    itemdictionary = {}
+    brokenatnotes = rawitem.partition("|")
+    if getname(brokenatnotes[0]):
+        itemdictionary["name"] = getname(brokenatnotes[0])
+    if getcontext(brokenatnotes[0]):
+        itemdictionary["context"] = getcontext(brokenatnotes[0])
+    if getstatus(brokenatnotes[0]):
+        itemdictionary["status"] = getstatus(brokenatnotes[0])
+    if getdays(brokenatnotes[0]):
+        itemdictionary["days"] = getdays(brokenatnotes[0])
+    if brokenatnotes[2]:
+        itemdictionary["note"] = brokenatnotes[2].strip()
+    print(itemdictionary)
+    return itemdictionary
+
+def getname(rawstring):
+    regex = r"-( \[.*\] | )(.*?)[@#|(]"
+    match = re.search(regex, rawstring)
+    if match:
+        name = match.group(2)
+    else:
+        name = ""
+    return name
+
+def getnote(rawstring):
+    note = rawstring.partition("|")[2].strip()
+    return note
+
+def getdays(rawstring):
+    regex = r"\(([^()]*)\)"
+    match = re.search(regex, rawstring)
+    if match:
+        days = match.group(1)
+    else:
+        days = ""
+    return days
+
+def getcontext(rawstring):
+    regex = r"(@[^\s]+)\s"
+    match = re.search(regex, rawstring)
+    if match:
+        context = match.group(0)
+    else:
+        context = ""
+    return context
+
+def statuslookup(x):
+    return {
+        ' ': "unmarked",
+        'x': "done",
+        'X': "done",
+        '!': "urgent",
+        '>': "migrated",
+        '<': "scheduled",
+        'O': "event",
+        'a': "assigned",
+        '-': "archived",
+        '?': "needs research",
+        '1': "1st priority",
+        '2': "2nd priority",
+        '3': "3rd priority",
+    }.get(x, x)
+#    }.get(x, "unknown mark")
+
+def getstatus(rawstring):
+    regex = r"\[(.*)\]"
+    match = re.search(regex, rawstring)
+    if match:
+        status = statuslookup(match.group(1))
+    else:
+        status = ""
+    return status
 
 #def f(x):
 #    return {
--- docs/_posts/2018-02-10-writing-a-parser-step-two.md
+++ docs/_posts/2018-02-10-writing-a-parser-step-two.md
@@ -8,3 +8,12 @@ tags: text, pvj, parser
 Next step running through a file with headings, ignoring the headings.
 
 http://json-schema.org/example/calendar.json
+https://developers.google.com/edu/python/strings
+https://www.dotnetperls.com/between-before-after-python
+https://scipython.com/blog/parenthesis-matching-in-python/
+http://thomas-cokelaer.info/tutorials/python/data_structures.html
+https://www.thegeekstuff.com/2014/07/python-regex-examples/
+https://docs.python.org/3/howto/regex.html#regex-howto
+https://www.regular-expressions.info/lookaround.html
+https://www.regular-expressions.info/python.html
+https://bytebaker.com/2008/11/03/switch-case-statement-in-python/
